a34984ad32a8ba9707a0f17a9cef1404a303556c
Madan <madanampera@hotmail.com>
2018-04-15 13:00:01 +1000

    Last Class
    Assignment 1

---
+++ Assignment/Assignment_1.py
@@ -0,0 +1,14 @@
+## Assignment 1
+## Create a console application which is Employee timesheet management portal.
+## The application should ask for user login (Only one user)
+## After successful login provide the user with two options
+    ## A. View timesheet data till now
+    ## B. Enter timesheet data (append data)
+## After each request wait for user input to represent the options or quit the application.
+
+##(Make use of CSV or JSON files to save timesheet data)
+## (Sample CSV timesheet data file:
+##  Date, Hours,
+##  9/4/2018, 8,
+##  10/4/2018,10,
+##  13/4/2018,7)
